9f3b078c74517d27f9e1f444bec36cbec5a525b3
superkid200 <minh306@outlook.com.vn>
2018-06-05 15:04:03 +0700

    Commit code to branch

--- Microsoft.Maker.IoT.Light.Library/Microsoft.Maker.IoT.Light.Library/LightStatus.cs
+++ Microsoft.Maker.IoT.Light.Library/Microsoft.Maker.IoT.Light.Library/LightStatus.cs
@@ -9,7 +9,7 @@ namespace Microsoft.Maker.IoT.Light.Library
 {
     public sealed class LightStatus
     {
-        public GpioPin pin { get; set; }
+        internal GpioPin pin { get; set; }
         private bool _isOn;
         public bool isOn { get { return _isOn; } set { if (value == true) { pin.Write(GpioPinValue.Low); } else {
                     pin.Write(GpioPinValue.High);
